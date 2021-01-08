7426de214defd11ce0a41e23963d63a86631d5d2
Gabriel Jensen <gabrielbjensen@icloud.com>
2021-01-08 19:38:06 +0100

    first

---
+++ main.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int main(int argc, char * * argv) {
+	if(argc < 0x2) {
+		printf("Missing argument \"file\".\n");
+		exit(EXIT_FAILURE);
+	}
+	if((access(argv[0x1], F_OK) == 0)) {
+		printf("The file exists.\n");
+	}
+	else {
+		printf("The file doesn't exist.\n");
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+}
